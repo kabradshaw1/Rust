b23d08be79933e1126c33ca7dc6bf02d11ffd7ff
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-10 00:23:36 -0400

    learning the things

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,16 @@
+use crate::token::Token;
+struct Lexer {
+  input: Vec<char>,
+}
+
+impl Lexer {
+  pub fn new(input: &str) -> Lexer {
+    todo!();
+  }
+  fn next_token() -> Token {
+    todo!();
+  }
+}
 #[cfg(test)]
 
 mod test {
@@ -13,6 +26,45 @@ mod test {
         kind: TokenKind::Assign,
         literal: "=".to_string(),
       },
+      Token{
+        kind: TokenKind::Plus,
+        literal: "+".to_string(),
+      },
+      Token{
+        kind: TokenKind::LParen,
+        literal: "(".to_string(),
+      },
+      Token{
+        kind: TokenKind::RParen,
+        literal: ")".to_string(),
+      },
+      Token{
+        kind: TokenKind::LBrace,
+        literal: "{".to_string(),
+      },
+      Token{
+        kind: TokenKind::RBrace,
+        literal: "}".to_string(),
+      },
+      Token{
+        kind: TokenKind::Comma,
+        literal: ",".to_string(),
+      },
+      Token{
+        kind: TokenKind::Semicolon,
+        literal: ";".to_string(),
+      },
+      Token{
+        kind: TokenKind::Eof,
+        literal: "".to_string(),
+      },
     ];
+
+    let lexer: Lexer = Lexer::new(input);
+
+    for (idx, exp_token) in expected.into_iter().enumerate() {
+      let recv_token: Token = lexer.next_token();
+      assert_eq!(exp_token.kind, recv_token.kind);
+    }
   }
 }
